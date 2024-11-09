a0ccd37e3552675ae9ba8210e5ef9a1a44b087b3
adrianssanz <sanzadrian@hotmail.es>
2024-11-09 19:40:48 +0000

    Añadida parte Front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordGeneratorComponent } from "./password-generator/password-generator.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswordGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
